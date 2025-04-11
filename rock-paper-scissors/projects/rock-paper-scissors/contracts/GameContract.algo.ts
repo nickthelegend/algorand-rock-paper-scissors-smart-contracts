5f2d198a2a049998ee112f2028b304c5f09c65d9
nickthelegend <82558763+nickthelegend@users.noreply.github.com>
2025-04-11 05:39:14 +0000

    Basic Contract

---
+++ rock-paper-scissors/projects/rock-paper-scissors/contracts/GameContract.algo.ts
@@ -0,0 +1,64 @@
+import { Contract } from '@algorandfoundation/tealscript';
+
+
+export class Fight extends Contract {
+
+
+    player1 = GlobalStateKey<Address>();
+  
+    player2 = GlobalStateKey<Address>();
+    depositedAmount = GlobalStateKey<uint64>();
+    maxDepositAmount = GlobalStateKey<uint64>();
+    winner = GlobalStateKey<Address>();
+
+
+    createApplication(player1: Address, player2: Address): void {
+
+        
+        this.player1.value = player1;
+        this.player2.value = player2;
+        this.depositedAmount.value = 0;
+        this.maxDepositAmount.value = 5;
+
+
+      }
+
+
+      depositfunds(ftransx : Txn){
+        assert(
+            this.txn.sender === this.app.creator ||
+              this.txn.sender === this.player1.value ||
+              this.txn.sender === this.player2.value
+          );
+
+          verifyPayTxn(ftransx, {
+            receiver: this.app.address,
+
+
+        });
+        this.depositedAmount.value += ftransx.assetAmount;
+
+
+
+
+      }
+      sendFunds (player: Address){
+
+        assert(
+            this.txn.sender === this.app.creator
+          );
+          sendPayment({
+            receiver: player,
+            amount: this.depositedAmount.value,
+          });
+          this.depositedAmount.value = 0;
+
+
+
+      }
+
+
+
+
+
+}
